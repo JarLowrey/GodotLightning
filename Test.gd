ae51ed53408b1eda28e58a66e8384d13ba764826
James Lowrey <jarlowrey@gmail.com>
2018-07-04 16:04:37 -0700

    not all tests is fade lightnnig

--- Test.gd
+++ Test.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _ready():
-	$FadedLightning1.apply_lightning(Vector2(0,800), 6, 400, .5, .5, .1)
+	$Lightning1.apply_lightning(Vector2(0,800), 6, 400, .5, .5, .1)
 	$FadedLightning2.apply_lightning(Vector2(0,100), 1, 20)
 	$FadedLightning3.apply_lightning(Vector2(0,100), 2, 100)
 	$FadedLightning4.apply_lightning(Vector2(0,800), 3, 100, .5, .5, .5)
@@ -13,7 +13,6 @@ func _ready():
 func _reset_lightning():
 	var curr_time = OS.get_ticks_msec() / 1000
 
-	$FadedLightning1.set_start_time(curr_time, 0.1)
 	$FadedLightning2.set_start_time(curr_time + .2, 0.1)
 	$FadedLightning3.set_start_time(curr_time + .1, 0.1)
 	$FadedLightning4.set_start_time(curr_time + .6, 0.1)
